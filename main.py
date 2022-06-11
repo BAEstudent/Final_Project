fdc7f86f1cb95193deaff98f7d6077ed59275ef5
BAE'24 Student <59344843+BAEstudent@users.noreply.github.com>
2022-06-11 15:39:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import plotly.graph_objects as go
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
 
 st.write('Final Project')
-
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get("https://lenta.com/allmarkets/")
-
-elements = driver.find_elements(by=By.TAG_NAME, value="li")
-for e in elements:
-    st.write(e.text)
