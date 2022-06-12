9ff4a0ec3ba60610dc7aab6747b614202e181e90
BAE'24 Student <59344843+BAEstudent@users.noreply.github.com>
2022-06-12 17:18:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import plotly.graph_objects as go
+import pandas_datareader.data as get_data
+import datetime
 
 st.write('Final Project')
 
+start_date = datetime.datetime(2017, 1, 1)
+end_date = datetime.datetime(2021, 12, 31)
+
+AAPL = get_data.get_data_yahoo("AAPL", start_date, end_date)
+st.dataframe(AAPL)
