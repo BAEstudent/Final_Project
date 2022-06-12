eed04bd111f3d6de37ec2bd4e5901ca7d421fc6e
BAE'24 Student <59344843+BAEstudent@users.noreply.github.com>
2022-06-12 14:00:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,23 @@ import math
 import plotly.graph_objects as go
 
 st.write('Final Project')
+
+@st.experimental_singleton
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from mytable;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
