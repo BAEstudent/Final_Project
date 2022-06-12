ab9a16d3ac1a6cd4c5cb81f6c1a2d2c40ebe11f2
BAE'24 Student <59344843+BAEstudent@users.noreply.github.com>
2022-06-12 14:20:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import plotly.graph_objects as go
+import sqlite3
 
 st.write('Final Project')
+
+conn = sqlite3.connect("database.sqlite")
+c = conn.cursor()
+c.execute(
+    """
+CREATE TABLE gradebook (
+id integer PRIMARY KEY,
+first_name text,
+last_name text,
+grade integer
+)
+"""
+)
+
+c.execute(
+    """
+SELECT * FROM gradebook;
+"""
+).fetchall()
